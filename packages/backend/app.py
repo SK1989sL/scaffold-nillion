c0fa571ac2a5c51411900cc9333ead6e3073dd4e
Nicholas Wehr <github@biochx.com>
2024-03-22 20:17:54 +0000

    validated docker service; it might have trouble sharing nodekey if it gets load (and spins up responders)

--- packages/backend/app.py
+++ packages/backend/app.py
@@ -1,9 +1,11 @@
+import base64
 import json
 import os
 import py_nillion_client
 import serverless_wsgi
 import subprocess
 import tempfile
+from waterbear import Bear
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -19,7 +21,7 @@ NILLION_NODE_SEED = os.environ.get("NILLION_NODE_SEED", "test-seed-0")
 
 with open(NILLION_CLUSTER_CONFIG, "r") as fp:
     config = json.load(fp)
-    config["payments_config"]["signer"]["wallet"]["private_key"] = NILLION_SERVICE_PK
+    config = Bear(**config)
 
 app = Flask(__name__)
 CORS(app)
@@ -27,20 +29,39 @@ CORS(app)
 
 @app.route("/upload-nada-source/<program_name>", methods=["POST"])
 async def upload_nada_source(program_name):
-    source = request.data.decode("utf-8")
+    data = request.get_json()
+    base64_nadasource = data["nadalang"]
+    source = base64.b64decode(base64_nadasource).decode("utf-8")
 
     with tempfile.NamedTemporaryFile(
         mode="w", dir="/tmp", suffix=".py", delete=False
     ) as temp_file:
         temp_file.write(source)
 
+    target_dir = os.path.dirname(temp_file.name)
     try:
-        subprocess.run([BIN_PYNADAC, temp_file.name], capture_output=True, text=True)
-    except subprocess.CalledProcessError as e:
-        error_message = f"pynadac execution failed: {e.returncode}, {e.stderr}"
-        return jsonify({"statusCode": 500, "body": {"error": error_message}})
+        result = subprocess.run(
+            [BIN_PYNADAC, "--target-dir", target_dir, temp_file.name],
+            capture_output=True,
+            text=True,
+        )
+        if "failed" in result.stderr.lower():
+            raise Exception("|".join([result.stderr, result.stdout]))
+    except Exception as e:
+        error_message = f"pynadac execution failed: [{e}]"
+        return jsonify({"statusCode": 400, "error": error_message})
 
-    compiled_program = os.path.splitext(temp_file.name)[0] + ".nada.bin"
+    only_file_name = os.path.basename(temp_file.name)
+    compiled_name = os.path.splitext(only_file_name)[0] + ".nada.bin"
+    compiled_program = os.path.join(target_dir, compiled_name)
+
+    payments_config = py_nillion_client.PaymentsConfig(
+        config.payments_config.rpc_endpoint,
+        NILLION_SERVICE_PK,
+        int(config.payments_config.signer.wallet.chain_id),
+        config.payments_config.smart_contract_addresses.payments,
+        config.payments_config.smart_contract_addresses.blinding_factors_manager,
+    )
 
     nodekey = py_nillion_client.NodeKey.from_seed(NILLION_NODE_SEED)
     userkey = py_nillion_client.UserKey.generate()
@@ -49,20 +70,21 @@ async def upload_nada_source(program_name):
         config.bootnodes,
         py_nillion_client.ConnectionMode.relay(),
         userkey,
-        config.payments_config,
+        payments_config,
     )
 
     result = await client.store_program(
         config.cluster_id, program_name, compiled_program
     )
-    return jsonify({"statusCode": 200, "body": {"message": result}})
+    return jsonify({"statusCode": 200, "programid": result})
 
 
 @app.route("/faucet/<address>", methods=["POST"])
 def faucet(address):
 
+    print(f"starting faucet for address {address}")
     try:
-        subprocess.run(
+        result = subprocess.run(
             [
                 BIN_CAST,
                 "send",
@@ -76,12 +98,15 @@ def faucet(address):
             ],
             capture_output=True,
             text=True,
+            shell=True,
         )
-    except subprocess.CalledProcessError as e:
-        error_message = f"pynadac execution failed: {e.returncode}, {e.stderr}"
-        return jsonify({"statusCode": 500, "body": {"error": error_message}})
+        if "failed" in result.stderr.lower():
+            raise Exception("|".join([result.stderr, result.stdout]))
+    except Exception as e:
+        error_message = f"pynadac execution failed: {e}"
+        return jsonify({"statusCode": 400, "error": error_message})
 
-    return jsonify({"statusCode": 200, "body": {"message": "OK"}})
+    return jsonify({"statusCode": 200, "message": "OK"})
 
 
 def handler(event, context):
